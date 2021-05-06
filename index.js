e9903dfbc9546f389476f55c419511d6d14949de
james <jameshoggard.dev@gmail.com>
2021-05-06 17:55:22 -0400

    commit tree 6c090f3082d165c6a3fedf47384c4f287649d86e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+const createEmployeeRecord = (array) =>{
+    return {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+const createEmployeeRecords = (array) => {
+    let new_arr = [];
+    array.forEach((record) => {
+        new_arr.push(createEmployeeRecord(record));
+    })
+    return new_arr;
+}
